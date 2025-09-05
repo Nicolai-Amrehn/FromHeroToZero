17cb7973fe85c3a2da7ba8bc010fc7c37e4e179f
Nicolai-Amrehn <nicolaiamrehn@posteo.de>
2025-09-05 16:23:03 +0200

    initial commit

---
+++ src/main/java/com/nra/FromHeroToZero/FromHeroToZeroApplication.java
@@ -0,0 +1,13 @@
+package com.nra.FromHeroToZero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FromHeroToZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FromHeroToZeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nra/FromHeroToZero/FromHeroToZeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nra.FromHeroToZero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromHeroToZeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
