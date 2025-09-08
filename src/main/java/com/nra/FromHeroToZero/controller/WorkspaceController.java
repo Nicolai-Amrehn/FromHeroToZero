56975d7acb71aca99481fdcbf3f2e4ebdf0733fe
Nicolai-Amrehn <nicolaiamrehn@posteo.de>
2025-09-08 13:34:57 +0200

    minor fix

--- src/main/java/com/nra/FromHeroToZero/controller/WorkspaceController.java
+++ src/main/java/com/nra/FromHeroToZero/controller/WorkspaceController.java
@@ -10,11 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
